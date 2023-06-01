571655a992be5c67c2cb27f9e3f1c149fde5dfcb
Rutu <113232070+Rutu-11@users.noreply.github.com>
2023-06-01 19:28:29 +0530

    vercel routes modified

--- routes/routes.js
+++ routes/routes.js
@@ -40,7 +40,7 @@ router.get('/', async (req, res) => {
 
 
 
-router.get('/', async (req, res) => {
+router.get('/csv', async (req, res) => {
   try {
 
     const submissions = await Submission.find()
