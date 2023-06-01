45618bbc676de0389e55c964d639a40a05d024af
Rutu <113232070+Rutu-11@users.noreply.github.com>
2023-06-01 17:19:09 +0530

    csv route added

--- routes/routes.js
+++ routes/routes.js
@@ -38,4 +38,28 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+
+router.get('/csv', async (req, res) => {
+  try {
+    // const page = parseInt(req.query.page) || 1;
+    // const limit = 5;
+    // const skip = (page - 1) * limit;
+
+    // const count = await Submission.countDocuments();
+    // const totalPages = Math.ceil(count / limit);
+
+    const submissions = await Submission.find()
+    .sort({ createdAt: -1 }) // Sort by creation date in descending order
+    // .skip(skip)
+    // .limit(limit);
+    res.json({ submissions });
+
+    
+  } catch (error) {
+    console.error('Error fetching submissions:', error);
+    res.status(500).json({ error: 'Failed to fetch submissions' });
+  }
+});
+
 module.exports = router;
